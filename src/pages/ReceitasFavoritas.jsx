c8bcddcc1368376b3898cfc48182a150232f2177
Henrique_Veras <theveras1@gmail.com>
2021-12-09 19:18:13 -0300

    [Grupo 7] - Alteraçao do header

--- src/pages/ReceitasFavoritas.jsx
+++ src/pages/ReceitasFavoritas.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ReceitasFavoritas() {
   return (
     <div>
       <Header pageName="Receitas Favoritas" />
-      <Footer />
     </div>
   );
 }
--- src/pages/ReceitasFeitas.jsx
+++ src/pages/ReceitasFeitas.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ReceitasFeitas() {
   return (
     <div>
       <Header pageName="Receitas Feitas" />
-      <Footer />
     </div>
   );
 }
